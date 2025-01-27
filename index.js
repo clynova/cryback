4e957e3b32acc8c2960f99df0714bc984f864664
Clynova <clynova.dev@gmail.com>
2025-01-26 22:24:16 -0300

    configuraciones iniciales

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hey')
+import dotenv from 'dotenv';
+import express from "express"
+import { conectarDB } from './config/db.js'
+
+dotenv.config()
+const { PORT } = process.env;
+
+const app = express()
+
+conectarDB()
+
+app.use('/', (req, res) => {
+    res.send('hey bro')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
+})
